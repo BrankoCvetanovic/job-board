ddea24de76138d23939b3753854292c03ecc9cf9
BahkoHatake <cbranko01@gmail.com>
2025-02-27 19:56:04 +0100

    Create job model

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('jobs', function (Blueprint $table) {
+        /* Schema::create('jobs', function (Blueprint $table) {
             $table->id();
             $table->string('queue')->index();
             $table->longText('payload');
@@ -42,16 +42,16 @@ return new class extends Migration
             $table->longText('payload');
             $table->longText('exception');
             $table->timestamp('failed_at')->useCurrent();
-        });
+        }); */
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+ /*    public function down(): void
     {
         Schema::dropIfExists('jobs');
         Schema::dropIfExists('job_batches');
         Schema::dropIfExists('failed_jobs');
-    }
+    } */
 };
